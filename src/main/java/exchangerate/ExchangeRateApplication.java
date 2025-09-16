cddfe24015e93587b7eed87cde542170b24595e5
JO-eusan <goodsan0330@gmail.com>
2025-09-16 13:31:58 +0900

    퀘스트3: IoC와 DI의 관계

--- src/main/java/exchangerate/ExchangeRateApplication.java
+++ src/main/java/exchangerate/ExchangeRateApplication.java
@@ -1,11 +1,14 @@
 package exchangerate;
 
-import exchangerate.decoupled.ExchangeRateSupportFactory;
+import exchangerate.decoupled.ExchangeRateRenderer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ExchangeRateApplication {
 
     public static void main(String[] args) {
-        ExchangeRateSupportFactory factory = ExchangeRateSupportFactory.getInstance();
-        factory.getExchangeRateRenderer().render();
+        final ApplicationContext context = new ClassPathXmlApplicationContext("exchange-rate-context.xml");
+        final ExchangeRateRenderer renderer = context.getBean("exchangeRateRenderer", ExchangeRateRenderer.class);
+        renderer.render();
     }
 }
