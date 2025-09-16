0193d14aa38a319a710c59dd073d7a88e4ac26a0
JO-eusan <goodsan0330@gmail.com>
2025-09-16 12:07:53 +0900

    퀘스트2: 제어의 역전

--- src/main/java/exchangerate/ExchangeRateApplication.java
+++ src/main/java/exchangerate/ExchangeRateApplication.java
@@ -1,16 +1,11 @@
 package exchangerate;
 
-import exchangerate.decoupled.ExchangeRateProvider;
-import exchangerate.decoupled.ExchangeRateRenderer;
-import exchangerate.decoupled.StandardInputExchangeRateProvider;
-import exchangerate.decoupled.StandardOutputExchangeRateRenderer;
+import exchangerate.decoupled.ExchangeRateSupportFactory;
 
 public class ExchangeRateApplication {
 
     public static void main(String[] args) {
-        final ExchangeRateProvider provider = new StandardInputExchangeRateProvider();
-        final ExchangeRateRenderer renderer = new StandardOutputExchangeRateRenderer();
-        renderer.setExchangeRateProvider(provider);
-        renderer.render();
+        ExchangeRateSupportFactory factory = ExchangeRateSupportFactory.getInstance();
+        factory.getExchangeRateRenderer().render();
     }
 }
--- src/main/java/exchangerate/decoupled/ExchangeRateSupportFactory.java
+++ src/main/java/exchangerate/decoupled/ExchangeRateSupportFactory.java
@@ -1,17 +1,31 @@
 package exchangerate.decoupled;
 
+import java.util.Properties;
+
 public final class ExchangeRateSupportFactory {
     private static final ExchangeRateSupportFactory INSTANCE;
     private final ExchangeRateProvider exchangeRateProvider;
     private final ExchangeRateRenderer exchangeRateRenderer;
 
     static {
-        INSTANCE = null;
+        INSTANCE = new ExchangeRateSupportFactory();
     }
 
     private ExchangeRateSupportFactory() {
-        this.exchangeRateProvider = null;
-        this.exchangeRateRenderer = null;
+        Properties properties = new Properties();
+        try {
+            final var resourceStream = this.getClass().getResourceAsStream("/exchange-rate.properties");
+            properties.load(resourceStream); // 불러온 파일을 properties 객체에 저장
+
+            final var providerClass = properties.getProperty("provider.class");
+            final var rendererClass = properties.getProperty("renderer.class");
+
+            this.exchangeRateProvider = (ExchangeRateProvider) Class.forName(providerClass).getDeclaredConstructor().newInstance();
+            this.exchangeRateRenderer = (ExchangeRateRenderer) Class.forName(rendererClass).getDeclaredConstructor().newInstance();
+            exchangeRateRenderer.setExchangeRateProvider(exchangeRateProvider);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 
     public static ExchangeRateSupportFactory getInstance() {
